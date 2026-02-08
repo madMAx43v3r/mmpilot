208e260b299b91ac4285164a0a85cac030987aa2
Max Wittal <max.wittal@mwittal.de>
2026-02-08 16:26:52 +0700

    test_camera

--- include/mmpilot/camera.h
+++ include/mmpilot/camera.h
@@ -8,17 +8,69 @@
 #ifndef INCLUDE_MMPILOT_CAMERA_H_
 #define INCLUDE_MMPILOT_CAMERA_H_
 
+#include <libcamera/libcamera.h>
+
+#include <mutex>
+#include <functional>
+
 
 namespace mmpilot {
 
 class Camera {
 public:
-	Camera();
+	struct Frame {
+		int width = 0;
+		int height = 0;
+		int stride = 0;
+		uint64_t sequence = 0;
+		uint64_t timestamp = 0;
+		std::string pixel_format;
+		std::vector<std::pair<void*, size_t>> data;
+	};
 
-	void run();
+	std::function<void(const Frame&)> on_frame;
+
+	Camera(int index, int stream, int width, int height, std::string pixel_format);
+
+	void open();
+
+	void start();
+
+	void stop();
+
+	std::string get_id() const;
+
+	libcamera::ControlList& controls() const;
+
+	static void init();
+
+	static void cleanup();
 
 private:
+	void handle(libcamera::Request* req);
+
+private:
+	int camera_index = 0;
+	int stream_index = 0;
+	int width = 0;
+	int height = 0;
+	int stride = 0;
+	int buffer_count = 0;
+
+	std::string pixel_format;
+
 	bool do_run = true;
+	libcamera::Stream* stream = nullptr;
+	std::shared_ptr<libcamera::Camera> cam;
+	std::unique_ptr<libcamera::ControlList> control;
+	std::unique_ptr<libcamera::CameraConfiguration> config;
+	std::unique_ptr<libcamera::FrameBufferAllocator> allocator;
+
+	std::mutex mutex;
+	std::vector<std::unique_ptr<libcamera::Request>> requests;
+	std::map<libcamera::FrameBuffer*, std::vector<std::pair<void*, size_t>>> mappings;
+
+	static std::shared_ptr<libcamera::CameraManager> g_manager;
 
 };
 
---
+++ test/test_camera.cpp
@@ -0,0 +1,73 @@
+/*
+ * test_camera.cpp
+ *
+ *  Created on: Feb 8, 2026
+ *      Author: mad
+ */
+
+#include <mmpilot/camera.h>
+
+#include <string>
+#include <iostream>
+
+using namespace mmpilot;
+
+
+uint64_t last_ts = 0;
+
+void handle(const Camera::Frame& frame)
+{
+	std::cout << "Frame " << frame.sequence << ": ts = " << frame.timestamp
+			<< ", width = " << frame.width << ", height = " << frame.height
+			<< ", stride = " << frame.stride
+			<< ", format = " << frame.pixel_format << std::endl;
+
+	for(const auto& buf : frame.data) {
+		std::cout << "  Buffer: size = " << buf.second << std::endl;
+	}
+	if(last_ts) {
+		std::cout << "  Interval: " << (frame.timestamp - last_ts) << " us" << std::endl;
+	}
+	last_ts = frame.timestamp;
+}
+
+
+int main(int argc, char** argv)
+{
+	int index			= argc > 1 ? atoi(argv[0]) : 0;
+	int width 			= argc > 2 ? atoi(argv[1]) : 1280;
+	int height 			= argc > 3 ? atoi(argv[2]) : 720;
+	int fps				= argc > 4 ? atoi(argv[3]) : 10;
+	std::string format	= argc > 5 ? argv[4] : "SRGGB8";
+
+	std::cout << "index = " << index << std::endl;
+	std::cout << "width = " << width << std::endl;
+	std::cout << "height = " << height << std::endl;
+	std::cout << "format = " << format << std::endl;
+
+	Camera::init();
+
+	Camera cam(index, 0, width, height, format);
+
+	cam.open();
+
+	cam.on_frame = &handle;
+
+	int frame_us = 1000000 / fps;
+	cam.controls().set(libcamera::controls::FrameDurationLimits,
+			libcamera::Span<const int64_t, 2>({frame_us, 2 * frame_us}));
+
+	cam.controls().set(libcamera::controls::ExposureTime, 5000);
+
+	cam.start();
+
+	std::string cmd;
+	std::cin >> cmd;
+
+	cam.stop();
+
+	Camera::cleanup();
+
+	return 0;
+}
+
