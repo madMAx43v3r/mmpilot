449aee8c82aa62517f4e2682e628aed1e5c695cc
Max Wittal <max.wittal@mwittal.de>
2026-03-03 04:14:14 +0700

    gyro att_window = 300

--- include/mmpilot/gyro.h
+++ include/mmpilot/gyro.h
@@ -43,7 +43,7 @@ public:
 	float gyro_scale = 1 / 16.4;				// [deg/s]
 	float accel_scale = 1 / 2048.f;				// [g]
 
-	float att_window = 100;		// how fast to converge towards attitude [sec]
+	float att_window = 300;		// how fast to converge towards attitude [sec]
 
 	Vec3f att_scale = Vec3f(0.1, 0.1, 1);		// [deg]
 
