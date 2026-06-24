023179c7fa2ca036876e1ef7a5e5e06324eb2a25
Max Wittal <max.wittal@mwittal.de>
2026-06-24 12:13:48 +0700

    test_msp_override

---
+++ test/test_msp_override.cpp
@@ -0,0 +1,31 @@
+/*
+ * test_msp_override.cpp
+ *
+ *  Created on: Jun 24, 2026
+ *      Author: mad
+ */
+
+#include <mmpilot/beta_msp.h>
+#include <thread>
+#include <array>
+
+using namespace mmpilot;
+
+int main()
+{
+	MSP2Client msp("/dev/ttyAMA0", 115200);
+
+	std::array<uint16_t, 8> rc = {
+		1500, // roll
+		1500, // pitch
+		1500, // yaw
+		1337, // throttle
+	};
+
+	while(true) {
+		msp.send_raw_rc(rc);
+		std::this_thread::sleep_for(std::chrono::milliseconds(20));
+	}
+}
+
+
