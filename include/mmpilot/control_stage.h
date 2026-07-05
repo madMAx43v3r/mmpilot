44f6290fec32ff004613a8dd9958b795566d1dd5
Max Wittal <max.wittal@mwittal.de>
2026-07-05 13:59:05 +0700

    gain tune

--- include/mmpilot/control_stage.h
+++ include/mmpilot/control_stage.h
@@ -23,12 +23,12 @@ public:
 	int max_angle = 200;		// RC offset
 	int max_throttle = 700;		// RC offset
 
-	float yaw_gain = 2;
-	float position_gain = 10;
+	float yaw_gain = 1;
+	float position_gain = 2;
 	float throttle_gain = 0.1;
 
 	float yawrate_gain = 1;
-	float velocity_gain = 1;
+	float velocity_gain = 2;
 	float vertical_gain = 0.1;
 
 	float AGL_min = 1;					// sanity limit [m]
